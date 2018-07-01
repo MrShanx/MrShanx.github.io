4ceef723d0ee3d38a69b9ec07cf82f7414749411
mrshanx <talan.emmanuel.test@gmail.com>
2018-07-01 00:36:41 -0400

    added navbar

---
+++ script/index.js
@@ -0,0 +1,56 @@
+//vanilla js for comptability
+var menu_button = document.querySelector(".sidenav-button_container_bars");
+var menu = document.getElementById("mySideNav");
+var close_button = document.querySelector(".sidenav-close-button_container");
+var main = document.getElementById("main");
+var document_window = window;
+var navbar = document.querySelector(".sidenav-button_container");
+var logo = document.querySelector(".App-menu-logo");
+var coffee_logo = document.querySelector(".coffee-logo");
+
+close_button.addEventListener("click", closeMenu);
+menu_button.addEventListener("click", openMenu);
+window.addEventListener("scroll", changeStickyMenu);
+
+function openMenu() {
+	//menu
+	menu.classList.add("open-nav"); //open-nav
+
+	//close button
+	close_button.classList.add("change"); //turns burger icon into close icon
+
+	//menu button - if menu is open, remove sticky menu and logo
+	navbar.classList.add("remove-button");
+}
+
+function closeMenu() {
+	//menu
+	menu.classList.remove("open-nav");
+
+	//close button
+	close_button.classList.remove("change"); //turns burger icon into close icon
+
+	//menu button - if menu is closed, add sticky menu
+	navbar.classList.remove("remove-button");
+}
+
+function changeStickyMenu() {
+	//past image
+	if (document.body.scrollTop > 290 || 
+			document.documentElement.scrollTop > 290) {
+		navbar.classList.add("sticky-menu_pastImage");
+		coffee_logo.classList.add("show-logo");
+	} else {
+		navbar.classList.remove("sticky-menu_pastImage");
+		coffee_logo.classList.remove("show-logo");
+	}
+
+	var window_width = window.innerWidth;
+	//past name
+	if ((document.body.scrollTop > 410 || 
+			document.documentElement.scrollTop > 410) && window_width > 400) {
+		logo.classList.add("show-logo");
+	} else {
+		logo.classList.remove("show-logo");
+	}
+} 
